f03e21ea704777a2126901f1c21f61cb7f75d856
Gram Liu <gram.liu226@gmail.com>
2022-12-09 18:22:25 -0500

    Separate parser logic between sections and ending

--- src/data/scraper/billHistory/billHistoryParser.ts
+++ src/data/scraper/billHistory/billHistoryParser.ts
@@ -7,27 +7,13 @@ export type BillHistory = Omit<
 >;
 type TableRow = ElementHandle<HTMLTableRowElement>;
 
-interface BaseSection {
+interface Section {
   field: keyof Omit<BillHistory, "committeeReferrals">;
-  fromEnd?: false;
-
   endPrefix?: string;
   prefix?: string;
   bold?: boolean;
   optional?: boolean;
 }
-
-interface StatusSection {
-  field: "committeeReferrals";
-  fromEnd: true;
-  endPrefix: string;
-
-  prefix?: string;
-  bold?: boolean;
-  optional?: boolean;
-}
-
-type Section = BaseSection | StatusSection;
 
 // Expected sections in the modal
 const sections: Section[] = [
@@ -37,8 +23,10 @@ const sections: Section[] = [
   { field: "abstract", prefix: "ABSTRACT", optional: true },
   { field: "dateFiled", prefix: "DATE FILED" },
   { field: "significance", prefix: "SIGNIFICANCE" },
-  { field: "committeeReferrals", fromEnd: true, endPrefix: "ACTIONS TAKEN" },
 ];
+
+const END_MARKER = "ACTIONS TAKEN";
+const END_FIELD = "committeeReferrals";
 
 /**
  * Extract text from a table row
@@ -61,6 +49,9 @@ async function extractTableRowText(
 
   // Remove invisible characters
   let cleaned = cellText.replace(/[\u0000-\u001F\u007F-\u009F]/g, "");
+
+  // Convert nbsp to space
+  cleaned = cellText.replace(/\u00a0/g, " ");
 
   // Remove leading/trailing quotes, if present
   if (cleaned.startsWith('"')) {
@@ -104,77 +95,95 @@ async function rowTextMatchesSection(
   text: string,
   section: Section
 ): Promise<boolean> {
-  if (section.fromEnd) {
-    return !text.startsWith(section.endPrefix);
-  } else {
-    return (
-      section.bold || // If bold, successful text extraction implies match
-      section.prefix == undefined || // If no prefix, automatic match
-      text.startsWith(section.prefix)
-    );
+  return (
+    section.bold || // If bold, successful text extraction implies match
+    section.prefix == undefined || // If no prefix, automatic match
+    text.startsWith(section.prefix)
+  );
+}
+
+async function parseSections(
+  rows: TableRow[]
+): Promise<[TableRow[], Partial<BillHistory>]> {
+  let acc = rows;
+  const billHistory = {} as Partial<BillHistory>;
+
+  for (const section of sections) {
+    while (acc.length > 0) {
+      // Get the next row from start
+      const row = acc.shift();
+      if (row == undefined) {
+        throw new Error("Unexpected undefined row!");
+      }
+
+      try {
+        const rowText = await extractTableRowText(row, section.bold ?? false);
+        const match = await rowTextMatchesSection(rowText, section);
+
+        if (match) {
+          // If match, set field in billHistory
+          const trimmed = trimPrefix(rowText as string);
+
+          // Parse significance into enum, as required
+          if (section.field === "significance") {
+            billHistory[section.field] = parseBillSignificance(trimmed);
+          } else {
+            billHistory[section.field] = trimmed;
+          }
+          break;
+        } else if (!section.optional) {
+          // If match not found and section is required, continue to next line
+          continue;
+        }
+      } catch (err) {
+        // Unable to parse text. Match failed
+        console.log("Unable to parse text");
+      }
+    }
+
+    if (!section.optional && acc.length == 0) {
+      throw new Error(`Missing required section: ${section.field}`);
+    }
   }
+
+  return [acc, billHistory];
+}
+
+async function parseEnd(rows: TableRow[]): Promise<Partial<BillHistory>> {
+  let acc = rows;
+  const billHistory = {} as Partial<BillHistory>;
+  const matching = [];
+  while (acc.length > 0) {
+    const row = acc.pop();
+    try {
+      const rowText = await extractTableRowText(row);
+
+      if (rowText.startsWith(END_MARKER)) {
+        // Marker reached. Break
+        break;
+      } else {
+        matching.push(rowText);
+      }
+    } catch (err) {
+      // Unable to parse text. Match failed
+      console.log("Unable to parse text");
+    }
+  }
+
+  billHistory[END_FIELD] = matching.reverse();
+  return billHistory;
 }
 
 export default async function parseBillHistoryRows(
   rows: TableRow[]
 ): Promise<BillHistory> {
-  let acc = rows;
-  const billHistory = {} as Partial<BillHistory>;
-  for (const section of sections) {
-    const matching = [];
-    while (acc.length > 0) {
-      // Get the next row from start or end
-      const row = section.fromEnd ? acc.pop() : acc.shift();
-      if (row == undefined) {
-        throw new Error("Unexpected undefined row!");
-      }
+  const [remainingRows, partialHistory] = await parseSections(rows);
+  const endHistory = await parseEnd(remainingRows);
 
-      let rowText: string | undefined;
-      let match = false;
-      try {
-        rowText = await extractTableRowText(row, section.bold ?? false);
-        match = await rowTextMatchesSection(rowText, section);
-      } catch (err) {
-        // Unable to parse text. Match failed
-        console.log("Unable to parse text");
-      }
-
-      if (match) {
-        // If match, add to match set
-        const trimmed = trimPrefix(rowText as string);
-        matching.push(trimmed);
-      } else if (section.fromEnd) {
-        // If from end, break once match no longer met
-        break;
-      } else if (!section.optional) {
-        // If match not found and section is expected, continue to next line
-        continue;
-      }
-
-      if (!section.fromEnd) {
-        // If not multiline and match found or section is optional
-        break;
-      }
-    }
-
-    if (!section.optional && matching.length == 0) {
-      throw new Error(`Missing required section: ${section.field}`);
-    }
-
-    if (!section.fromEnd) {
-      // Push single value
-      const [value] = matching as [string];
-
-      if (section.field === "significance") {
-        billHistory[section.field] = parseBillSignificance(value);
-      } else {
-        billHistory[section.field] = value;
-      }
-    } else {
-      // If fromEnd, reverse and push all values
-      billHistory[section.field] = matching.reverse();
-    }
-  }
+  const billHistory = {
+    ...partialHistory,
+    ...endHistory,
+  };
 
   return billHistory as BillHistory;
 }
