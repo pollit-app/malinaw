ea797f7ac20d9c0bb4721972151cb64d767658b3
Gram Liu <gram.liu226@gmail.com>
2022-12-16 05:10:29 -0500

    Return politician name in parseBillAuthorships

--- src/data/scraper/congressBillAuthorships/billAuthorshipParser.ts
+++ src/data/scraper/congressBillAuthorships/billAuthorshipParser.ts
@@ -1,18 +1,30 @@
 import axios from "axios";
+import expect from "expect";
 import { parse } from "node-html-parser";
+import { parseRepresentativeTitle } from "../representatives/parseProfile";
+
+export interface BillAuthorships {
+  name: string;
+  principal: string[];
+  coAuthored: string[];
+}
 
 /**
- * Given the url of a member of congress, returns two string lists:
- * bills principally authored and bills co-authored
+ * Given the url of a member of congress, returns the name of the politician,
+ * bills principally authored, and bills co-authored
  *
  * Example url: https://www.congress.gov.ph/members/search.php?id=abalos
  */
 export default async function parseBillAuthorships(
   url: string
-): Promise<[string[], string[]]> {
-  console.log("Parsing url", url);
+): Promise<BillAuthorships> {
   const { data } = await axios.get(url);
   const root = parse(data);
+
+  // Extract name
+  const titleContainer = root!.querySelector(".text-primary");
+  expect(titleContainer).not.toBeNull();
+  const { name } = parseRepresentativeTitle(titleContainer!);
 
   // Identify relevant lines
   const nodes = root!.querySelectorAll("h4, .panel-heading");
@@ -36,5 +48,9 @@ export default async function parseBillAuthorships(
   const principalBills = principalLines.map((node) => node.innerText);
   const coAuthorBills = coAuthorLines.map((node) => node.innerText);
 
-  return [principalBills, coAuthorBills];
+  return {
+    name,
+    principal: principalBills,
+    coAuthored: coAuthorBills,
+  };
 }
